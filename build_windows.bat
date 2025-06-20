f1e5bc262d1ae157321699f3642e0accf16ee831
Yon Liud <ejliud@gmail.com>
2025-06-20 12:42:42 +0300

    refactor: simplify build script to only run pyinstaller

--- build_windows.bat
+++ build_windows.bat
@@ -1,28 +1,15 @@
 @echo off
 cls
 
-:: Get the current git hash
-for /f %%i in ('git rev-parse --short HEAD') do set GIT_HASH=%%i
-echo Current git hash: %GIT_HASH%
-
-:: Create version files
-echo %GIT_HASH% > frontend\version.txt
-echo %GIT_HASH% > backend\version.txt
-
-:: Set build mode
-set RELEASE_BUILD=true
-
 :: Build frontend
-echo BUILDING FRONTEND
 cd frontend
 pyinstaller --noconfirm main.spec
 cd ..
 
 :: Build backend
-echo BUILDING BACKEND
 cd backend
 pyinstaller --noconfirm server.spec
 cd ..
 
-echo Build completed with version: %GIT_HASH%
+echo Build completed.
 pause
